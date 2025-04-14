2420fdf42aa4f8933f23291c1e3b7c9b40c65bcd
victor-takayama <123839060+victor-takayama@users.noreply.github.com>
2025-04-14 19:11:18 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import React from 'react';
 import HeroSection from './components/HeroSection';
 import FormularioLeads from './components/FormularioLeads';
+import ComoFunciona from './components/ComoFunciona';
 
 function App() {
   return (
     <div>
       <HeroSection />
       <FormularioLeads />
+      <ComoFunciona />
     </div>
   );
 }
-
-export default App;
