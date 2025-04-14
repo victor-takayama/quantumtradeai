b0419f7a2b1d2aec098e8858927f09ab3f86e6b8
victor-takayama <123839060+victor-takayama@users.noreply.github.com>
2025-04-14 19:54:14 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import HeroSection from './components/HeroSection';
 import FormularioLeads from './components/FormularioLeads';
 import ComoFunciona from './components/ComoFunciona';
@@ -6,10 +8,12 @@ import ResultadosSeguranca from './components/ResultadosSeguranca';
 import Monetizacao from './components/Monetizacao';
 import SuporteComunidade from './components/SuporteComunidade';
 import Rodape from './components/Rodape';
+import TermosDeUso from './components/TermosDeUso';
+import PoliticaPrivacidade from './components/PoliticaPrivacidade';
 
-function App() {
+function Home() {
   return (
-    <div>
+    <>
       <HeroSection />
       <FormularioLeads />
       <ComoFunciona />
@@ -17,7 +21,20 @@ function App() {
       <Monetizacao />
       <SuporteComunidade />
       <Rodape />
-    </div>
+    </>
   );
 }
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/termos" element={<TermosDeUso />} />
+        <Route path="/privacidade" element={<PoliticaPrivacidade />} />
+      </Routes>
+    </Router>
+  );
+}
+
 export default App;
