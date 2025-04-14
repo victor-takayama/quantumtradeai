e4b00b4205b8ba4cc933cf06088339ace52f96c5
victor-takayama <123839060+victor-takayama@users.noreply.github.com>
2025-04-14 19:20:50 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import HeroSection from './components/HeroSection';
 import FormularioLeads from './components/FormularioLeads';
 import ComoFunciona from './components/ComoFunciona';
 
+import ResultadosSeguranca from './components/ResultadosSeguranca';
+
 function App() {
   return (
     <div>
       <HeroSection />
       <FormularioLeads />
       <ComoFunciona />
+      <ResultadosSeguranca />
     </div>
   );
 }
